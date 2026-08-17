f76f08c1c6ee3d1e1416b994f90ff3faf6b4e794
Sung-jinwo <eduardomendez15120@gmail.com>
2026-08-17 15:54:14 -0500

    Actualiza proyecto

---
+++ src/main/java/com/backend/nutri_predic/NutriPredicApplication.java
@@ -0,0 +1,13 @@
+package com.backend.nutri_predic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriPredicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriPredicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/nutri_predic/NutriPredicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.nutri_predic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriPredicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
